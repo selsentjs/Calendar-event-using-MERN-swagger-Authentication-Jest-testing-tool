797c3ab9d0a3fe1cf9ac3c03d66a3828480624c7
selvisentest2006 <selvisentest2006@gmail.com>
2025-02-22 22:38:49 +0530

    statuscode added

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,13 +1,14 @@
 const Event = require("../Model/Calendar");
+const { StatusCodes } = require('http-status-codes');
 
 // get all the events
 const getAllEvents = async (req, res) => {
   try {
     const event = await Event.find().populate("userId", "name");
-    res.status(200).json({ event });
+    return res.status(StatusCodes.OK).json({ event });
   } catch (err) {
     console.error("server error:", err);
-    res.status(500).json({ msg: "server error", err });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "server error", err });
   }
 };
 
@@ -17,12 +18,12 @@ const getSingleEvent = async (req, res) => {
   try {
     const event = await Event.findOne({ _id: id });
     if (!event) {
-      return res.status(404).json({ msg: "Event not found" });
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: "Event not found" });
     }
-    res.status(200).json({ event });
+    return res.status(StatusCodes.OK).json({ event });
   } catch (err) {
     console.error("server error:", err);
-    res.status(500).json({ msg: "server error", err });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "server error", err });
   }
 };
 
@@ -47,25 +48,30 @@ const createEvent = async (req, res) => {
       userId,
     });
 
-    res.status(200).json({ msg: "Event created", event });
+    return res.status(StatusCodes.OK).json({ msg: "Event created", event });
   } catch (err) {
     console.error("server error:", err);
-    res.status(500).json({ msg: "server error", err });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "server error", err });
   }
 };
 // update event
 const updateEvent = async (req, res) => {
   const { id } = req.params;
+  
   try {
     // Use { new: true } to return the updated event, not the original one
     const event = await Event.findOneAndUpdate({ _id: id }, req.body, { new: true });
     if (!event) {
-      return res.status(404).json({ msg: "Event not found" });
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: "Event not found" });
     }
-    res.status(200).json({ msg: "event updated", event });
+     // Ensure the logged-in user is the owner of the event
+     if (event.userId.toString() !== req.user.userId) {
+      return res.status(StatusCodes.FORBIDDEN).json({ message: 'You do not have permission to update this event' });
+    }
+    return res.status(StatusCodes.OK).json({ msg: "event updated", event });
   } catch (err) {
     console.error("server error:", err);
-    res.status(500).json({ msg: "server error", err });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "server error", err });
   }
 };
 // delete event
@@ -74,12 +80,12 @@ const deleteEvent = async (req, res) => {
   try {
     const event = await Event.findOneAndDelete({ _id: id }, req.body);
     if (!event) {
-      return res.status(404).json({ msg: "Event not found" });
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: "Event not found" });
     }
-    res.status(200).json({ msg: "event deleted", event });
+    return res.status(StatusCodes.OK).json({ msg: "event deleted", event });
   } catch (err) {
     console.error("server error:", err);
-    res.status(500).json({ msg: "server error", err });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "server error", err });
   }
 };
 
